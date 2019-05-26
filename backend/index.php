ff55acf1b320b562a5ff34c8d24c77ad9e01f4b6
vu17b017 <vu17b017@technikum-wien.at>
2019-05-26 08:56:24 +0200

    register und savetrack am server hat funktioniert

--- backend/index.php
+++ backend/index.php
@@ -1,9 +1,21 @@
 <html>
 <head>
-<title>Test send track</title>
+<title>Test</title>
 </head>
 
 <body>
+
+<h1>Test insert track SERVER</h1>
+<form name="eingabe" action="https://ieslamp.technikum-wien.at/bvu19sys5/jamlocal/savetrack.php" method="POST"></p>
+<p>Track Json: <input type="text" name="json"></p>
+<input type="submit" name="send" value="Send">
+</form>
+
+<h1>Test insert track LOCALHOST</h1>
+<form name="eingabe" action="http://localhost/jamlocal/savetrack.php" method="POST"></p>
+<p>Track Json: <input type="text" name="json"></p>
+<input type="submit" name="send" value="Send">
+</form>
 
 <h1>Test register user SERVER</h1>
 <form name="eingabe" action="https://ieslamp.technikum-wien.at/bvu19sys5/jamlocal/register.php" method="POST"></p>
