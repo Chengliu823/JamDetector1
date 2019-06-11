30aef3b49f93aea13aa0225d86a525c5a4d79966
vu17b017 <vu17b017@technikum-wien.at>
2019-06-11 20:10:17 +0200

    setting ist schöner

--- app/src/main/java/com/example/semesterprojekt/AboutUSActivity.java
+++ app/src/main/java/com/example/semesterprojekt/AboutUSActivity.java
@@ -2,12 +2,32 @@ package com.example.semesterprojekt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class AboutUSActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        text = (TextView) findViewById(R.id.textView1);
+        text.setText("Wir sind Studierende des von Verkehr und Umwelt\n" +
+                "\n" +
+                "Kontakt:\n" +
+                "Frau Peres Ferreira: vu17b038@technikum-wien.at \n" +
+                "Herr Liu: vu17b026@technikum-wien.at \n" +
+                "Herr Wagner: vu17b017@technikum-wien.at\n" +
+                "\n" +
+                "Adresse\n" +
+                "Fachhochschule Technikum Wien \n" +
+                "Höchstädtplatz 6 \n" +
+                "1200 Wien \n" +
+                "\n" +
+                "© WPL Technologie.");
     }
 }
--- app/src/main/java/com/example/semesterprojekt/HelpActivity.java
+++ app/src/main/java/com/example/semesterprojekt/HelpActivity.java
@@ -2,12 +2,30 @@ package com.example.semesterprojekt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        text = (TextView) findViewById(R.id.textView1);
+        text.setText("Wir sind Studierende des von Verkehr und Umwelt\n" +
+                "\n" +
+                "Kontakt:\n" +
+                "Frau Peres Ferreira: vu17b038@technikum-wien.at \n" +
+                "Herr Liu: vu17b026@technikum-wien.at \n" +
+                "Herr Wagner: vu17b017@technikum-wien.at\n" +
+                "\n" +
+                "Adresse\n" +
+                "Fachhochschule Technikum Wien \n" +
+                "Höchstädtplatz 6 \n" +
+                "1200 Wien \n" +
+                "\n" +
+                "© WPL Technologie.");
     }
 }
